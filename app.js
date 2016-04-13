42dc40611e672e9d2145f8f8c66ebd7ea406122b
Mark Karavan <mark.karavan@gmail.com>
2016-04-12 21:14:08 -0400

    adds functional style

--- app.js
+++ app.js
@@ -5,53 +5,70 @@ var app = express();
 
 app.get('/', function (req, res) {
 
-	var retObj = {results: []};
+	var retJson = {results: []};
 	var query = req.url.slice(3);
+	
 	console.log(query);
 
-	request('https://www.google.com/search?q=' + query + '&rct=j', function (error, response, html) {
+	var engines = [
+		{
+			engine: "Google",
+			queryString: "https://www.google.com/search?q=QUERYHERE&rct=j",
+			searchElement: 'h3.r',
+			title : function (obj) {
+				return obj.text();
+			},
+			href: function (obj) {
+				return obj.find('a').attr('href');
+			}
+		},
+		{
+			engine: "Bing",
+			queryString:  "https://www.bing.com/search?q=QUERYHERE",
+			searchElement: 'li.b_algo',
+			title : function (obj) {
+				return obj.find('a').text();
+			},
+			href: function (obj) {
+				return obj.find('a').attr('href');
+			}
+		},
+		{
+			engine: "Yahoo",
+			queryString: "https://search.yahoo.com/search;_ylc=X3oDMTFiN25laTRvBF9TAzIwMjM1MzgwNzUEaXRjAzEEc2VjA3NyY2hfcWEEc2xrA3NyY2h3ZWI-?p=QUERYHERE&fr=yfp-t-201&fp=1&toggle=1&cop=mss&ei=UTF-8",
+			searchElement: 'div.algo',
+			title : function (obj) {
+				return obj.find('h3 a').text();
+			},
+			href: function (obj) {
+				return obj.find('h3 a').attr('href');
+			}			
+		}
+	];
+
+	var pullFromEngine = function (engData, i, json, q) {
+		var e = engData[i];
+		var queryString = e.queryString.replace("QUERYHERE", q);
+		request(queryString, function (error, response, html) {
 			if (!error && response.statusCode == 200) {
 				var $ = cheerio.load(html);
-				$('h3.r').each(function(i, element){
-					var item = {}
-					var a = $(this);
-					item.title = a.text();
-					item.engine = "Google";
-					item.href = $(this).find('a').attr('href');
-		      		retObj.results.push(item);
+				$(e.searchElement).each(function(i, element) {
+					var item = {};
+					item.engine = e.engine;
+					item.title = e.title($(this));
+					item.href = e.href($(this));
+					json.results.push(item);
 				});
-			}
-
-		  	request('https://www.bing.com/search?q=' + query, function (error, response, html) {
-				if (!error && response.statusCode == 200) {
-					var $ = cheerio.load(html);
-					$('li.b_algo').each(function(i, element){
-						var item = {}
-						var a = $(this).find('a').text();
-						item.title = a;
-						item.engine = "Bing";
-						item.href = $(this).find('a').attr('href');
-			      		retObj.results.push(item);
-					});
-				  	request('https://search.yahoo.com/search;_ylc=X3oDMTFiN25laTRvBF9TAzIwMjM1MzgwNzUEaXRjAzEEc2VjA3NyY2hfcWEEc2xrA3NyY2h3ZWI-?p=' + query + '&fr=yfp-t-201&fp=1&toggle=1&cop=mss&ei=UTF-8', function (error, response, html) {
-						if (!error && response.statusCode == 200) {
-							var $ = cheerio.load(html);
-							$('div.algo').each(function(i, element){
-								var item = {}
-								var a = $(this).find('h3 a');
-								item.title = a.text();
-								item.engine = "Yahoo";
-								item.href = a.attr('href');
-					      		retObj.results.push(item);
-							});
-							res.send(retObj);
-						}
-					});
+				if (i >= (engData.length - 1)) {
+					res.send(json);
+				} else {
+					pullFromEngine(engData, i + 1, json, q);
 				}
-			});
-
+			}
 		});
-	
+	};
+
+	pullFromEngine(engines, 0, retJson, query);
 });
 
 app.listen(1234, function () {
